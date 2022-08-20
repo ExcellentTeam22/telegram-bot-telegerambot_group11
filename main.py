598aedcd12bcdf1b52524e08f5419a686f54fa77
Ortal Yatzkan <ortalyatzkan@gmail.com>
2022-08-21 02:20:17 +0300

    Add function that send a record

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import math
+import os
 
 from flask import Flask, Response, request
 import requests
+import http.client
+from urllib.request import urlretrieve
+import webbrowser
+
 
 app = Flask(__name__)
 
-TOKEN = '5694196769:AAGwRmFwMp89-0ehKqjXwJMVv9-THuuq_ME'
-TELEGRAM_INIT_WEBHOOK_URL = 'https://api.telegram.org/bot{}/setWebhook?url=https://23bb-46-31-101-77.eu.ngrok.io/message'.format(TOKEN)
-
+TOKEN = '5767542973:AAFFdd3BBNJodwNUwJGKJ_AyuvI_mTis8f8'
+TELEGRAM_INIT_WEBHOOK_URL = 'https://api.telegram.org/bot{}/setWebhook?url=https://4c72-93-173-125-118.eu.ngrok.io/sendAudio'.format(TOKEN)
 requests.get(TELEGRAM_INIT_WEBHOOK_URL)
+querystring = {"key": "834f306bed0e4f2da7bb335b01b0bde6"}#key of record API
 
 
 @app.route('/sanity')
@@ -19,99 +24,39 @@ def sanity(): return "Server is running"
 def p(): return "Server is running"
 
 
-def is_prime(n: int) -> int:
-    for i in range(2, n):
-        if (n%i) == 0:
-            return False
-    return True
-
-
-def is_factorial(n: int) -> int:
-    i = 1
-    while True:
-        if n % i == 0:
-            n //= i
-        else:
-            break
-
-        i += 1;
-    if n == 1:
-        return True
-    else:
-        return False
-
-
-def is_palindrome(s: int) -> int:
-    return s == s[::-1]
-
-
-def keywithmaxval(counter_command:dict) -> str:
-    """ a) create a list of the dict's keys and values;
-        b) return the key with the max value"""
-    v = list(counter_command.values())
-    k = list(counter_command.keys())
-    return k[v.index(max(v))]
-
-
-@app.route('/message', methods=["POST"])
+@app.route('/sendAudio', methods=["POST"])
 def handle_message():
-    counter_command = {'factorial': 0, 'palindrome': 0, 'sqrt': 0, 'prime': 0}
     chat = request.get_json()
     chat_id = chat['message']['chat']['id']
     text = chat['message']['text']
     print(chat)
 
-    if text == 'hello':
-        res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'"
-                           .format(TOKEN, chat_id, "how are you?"))
-    if text.startswith("/prime"):
-        split_text = text.split()
-        if is_prime(int(text.split()[1])):
-            res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'"
-                                   .format(TOKEN, chat_id, "is prime"))
-        else:
-            res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'"
-                                   .format(TOKEN, chat_id, "not prime"))
-        counter_command['prime'] += 1
+    filename = 'speech.wav'
 
-    if text.startswith("/factorial"):
-        split_text = text.split()
-        print(split_text)
-        if is_factorial(int(text.split()[1])):
-            res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'"
-                               .format(TOKEN, chat_id, "True"))
-        else:
-            res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'"
-                               .format(TOKEN, chat_id, "False"))
-        counter_command['factorial'] += 1
+    url = "https://voicerss-text-to-speech.p.rapidapi.com/"
 
-    if text.startswith("/palindrome"):
-        split_text = text.split()
-        print(split_text)
-        if is_palindrome(text.split()[1]):
-            res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'"
-                               .format(TOKEN, chat_id, "True"))
-        else:
-            res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'"
-                               .format(TOKEN, chat_id, "False"))
-        counter_command['factorial'] += 1
+    headers = {
+        "content-type": "application/x-www-form-urlencoded",
+        "X-RapidAPI-Key": "5991e19cd9msh6ba7b3e15ad8bafp162e99jsn85520f9f925c",
+        "X-RapidAPI-Host": "voicerss-text-to-speech.p.rapidapi.com"
+    }
 
-    if text.startswith("/sqrt"):
-        split_text = text.split()
-        if math.isqrt(text.split()[1]):
-            res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'"
-                               .format(TOKEN, chat_id, "True"))
-        else:
-            res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'"
-                               .format(TOKEN, chat_id, "False"))
-        counter_command['sqrt'] += 1
+    urlretrieve("http://api.voicerss.org/?key=834f306bed0e4f2da7bb335b01b0bde6&hl=en-us&c=MP3&f=16khz_16bit_stereo&src=Hello,world!",filename)
 
-    if text.startswith("/popular"):
-        maximum = max(counter_command.values())
-        result = filter(lambda x: x[1] == maximum, counter_command.items())
+    os.startfile(filename)  # play the file using default application
+    with open('speech.wav', 'rb') as audio:
+        payload = {
+            'chat_id': chat_id,
+            'title': 'file.wav',
+            'parse_mode': 'HTML'
+        }
 
-        res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'"
-                               .format(TOKEN, chat_id,))
+        files = {
+            'audio' : open('speech.wav','rb')
+        }
+        resp = requests.post(
+            "https://api.telegram.org/bot{token}/sendAudio".format(token=TOKEN),
+            data=payload,files=files)
     return Response("success")
 
 
